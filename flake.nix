0b0e00edd779b5d2b207777a64273d6b41dccd38
AlxTray <alex.ashby02@icloud.com>
2024-08-01 13:01:28 +0100

    chore: added direnv dependencies to repo

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nix-flake-based environment for required dependencies";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      javaVersion = 21;
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      overlays.default =
+        final: prev: rec {
+          jdk = prev."jdk${toString javaVersion}";
+          maven = prev.maven.override { java = jdk; };
+        };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ maven nodejs ];
+        };
+      });
+    };
+}
